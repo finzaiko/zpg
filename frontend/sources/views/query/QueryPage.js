a9233d798d636474b9162d112d42e4a6f4171357
Arifin <arifin@citratubindo.com>
2022-11-02 14:55:24 +0700

    upd hightligted json result

--- frontend/sources/views/query/QueryPage.js
+++ frontend/sources/views/query/QueryPage.js
@@ -27,6 +27,26 @@ function truncateString(str, n) {
   return str.length > n ? str.slice(0, n - 1) + "&nbsp;" : str; //&hellip;
 }
 
+function syntaxHighlight(json) {
+  // https://stackoverflow.com/questions/4810841/pretty-print-json-using-javascript/7220510#7220510
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+      var cls = 'number';
+      if (/^"/.test(match)) {
+          if (/:$/.test(match)) {
+              cls = 'key';
+          } else {
+              cls = 'string';
+          }
+      } else if (/true|false/.test(match)) {
+          cls = 'boolean';
+      } else if (/null/.test(match)) {
+          cls = 'null';
+      }
+      return '<span class="' + cls + '">' + match + '</span>';
+  });
+}
+
 function newQueryTab() {
   function isInt(value) {
     var x;
@@ -1525,10 +1545,15 @@ export function QueryPage(prefix, selectedDb) {
                 },
               ],
             },
+            // body: {
+            //   view: "textarea",
+            //   css: "z_query_detail_cell",
+            //   value: content,
+            // },
             body: {
-              view: "textarea",
+              view: "template",
               css: "z_query_detail_cell",
-              value: content,
+              template: `<pre style='height:100%;overflow: auto;'>${syntaxHighlight(content)}</pre>`,
             },
           })
           .show();

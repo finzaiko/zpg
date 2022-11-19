2f7102e3fe99938ebcad9db878e8498753964b7f
Arifin <arifin@citratubindo.com>
2022-11-19 22:13:17 +0700

    add first loading

--- frontend/sources/myapp.js
+++ frontend/sources/myapp.js
@@ -15,27 +15,29 @@ import { showExpiredMsg } from "./helpers/ui";
 //   });
 // }
 
-webix.protoUI({
-  name:"fadeInWindow",
-  $init:function(){
-
-    this.$ready.push(function(){
-      this.attachEvent("onShow", function(){
-          const boxes = document.querySelectorAll('body > div.webix_modal');
-          boxes.forEach(box => {
-            box.style.backgroundColor = '#FFF';
+webix.protoUI(
+  {
+    name: "fadeInWindow",
+    $init: function () {
+      this.$ready.push(function () {
+        this.attachEvent("onShow", function () {
+          const boxes = document.querySelectorAll("body > div.webix_modal");
+          boxes.forEach((box) => {
+            box.style.backgroundColor = "#FFF";
           });
-        this.$view.className = this.$view.className + " animate__animated animate__bounceInDown";  // animate__fadeInDown
-      })
-      this.attachEvent("onHide", function(){
-        this.$view.style.display = "block";
-        this.$view.className += " animate__fadeOut";
-      })
-    });
-  }
-}, webix.ui.window);
+          this.$view.className =
+            this.$view.className + " animate__animated animate__bounceInDown"; // animate__fadeInDown
+        });
+        this.attachEvent("onHide", function () {
+          this.$view.style.display = "block";
+          this.$view.className += " animate__fadeOut";
+        });
+      });
+    },
+  },
+  webix.ui.window
+);
 // https://snippet.webix.com/mw0mlhvz
-
 
 export default class MyApp extends JetApp {
   constructor(config) {
@@ -67,128 +69,141 @@ if (!BUILD_AS_MODULE) {
   // webix.debug({events: true});
   // webix.ready(() => new MyApp().render() );
   webix.ready(() => {
-    const app = new MyApp();
-    let appUrl;
-    app.on("app:guard", function (url, view, nav) {
-      state.navUrl = nav;
-      appUrl = nav;
-      if (url == "/p") nav.redirect = "/p/start";
+    const laodingDelay = 800; // ms
+    document.querySelectorAll(".zl").forEach(function (node) {
+      setTimeout(() => {
+        node.classList.add("zfo");
+        node.remove();
+      }, laodingDelay);
     });
 
-    let hasReload = false;
+    const app = new MyApp();
 
-    // Prevent reload page when query editor dirty
-    window.onbeforeunload = function (e) {
-      e = e || window.event;
-      let sum = 0;
-      for (let i = 0; i < stateQuery.countPage + 1; i++) {
-        if (i > 0) {
-          if ($$("z_query_" + i + "_sql_editor")) {
-            const a = $$("z_query_" + i + "_sql_editor").getValue().length;
-            sum += a;
-          }
-        } else {
-          if ($$("z_query_sql_editor")) {
-            const b = $$("z_query_sql_editor").getValue().length;
-            sum += b;
+    setTimeout(() => {
+      let appUrl;
+      app.on("app:guard", function (url, view, nav) {
+        state.navUrl = nav;
+        appUrl = nav;
+        if (url == "/p") nav.redirect = "/p/start";
+      });
+
+      let hasReload = false;
+
+      // Prevent reload page when query editor dirty
+      window.onbeforeunload = function (e) {
+        e = e || window.event;
+        let sum = 0;
+        for (let i = 0; i < stateQuery.countPage + 1; i++) {
+          if (i > 0) {
+            if ($$("z_query_" + i + "_sql_editor")) {
+              const a = $$("z_query_" + i + "_sql_editor").getValue().length;
+              sum += a;
+            }
+          } else {
+            if ($$("z_query_sql_editor")) {
+              const b = $$("z_query_sql_editor").getValue().length;
+              sum += b;
+            }
           }
         }
-      }
-      if (sum > 0) {
-        // // For IE and Firefox prior to version 4
-        if (e) {
-          e.returnValue = "Sure?";
+        if (sum > 0) {
+          // // For IE and Firefox prior to version 4
+          if (e) {
+            e.returnValue = "Sure?";
+          }
+          // // For Safari
+          return "Sure?";
         }
-        // // For Safari
-        return "Sure?";
-      }
-    };
+      };
 
-  //   if (window.history && window.history.pushState) {
-  //     console.log("asdfasdf");
-  //     window.addEventListener("popstate", (event) => {
-  //       console.log(
-  //         "location: " +
-  //           document.location +
-  //           ", state: " +
-  //           JSON.stringify(event.state)
-  //       );
-  //       // webix.confirm("Are you sure?").then((result) => {
-  //       //   if (result) {
-  //       //     history.back();
-  //       //   }
-  //       // });
-  //       // history.go(1);
-  //       history.pushState(null, null, window.location.href);
-  //     });
-  //   }
+      //   if (window.history && window.history.pushState) {
+      //     console.log("asdfasdf");
+      //     window.addEventListener("popstate", (event) => {
+      //       console.log(
+      //         "location: " +
+      //           document.location +
+      //           ", state: " +
+      //           JSON.stringify(event.state)
+      //       );
+      //       // webix.confirm("Are you sure?").then((result) => {
+      //       //   if (result) {
+      //       //     history.back();
+      //       //   }
+      //       // });
+      //       // history.go(1);
+      //       history.pushState(null, null, window.location.href);
+      //     });
+      //   }
 
-  //   // window.onbeforeunload = function() { return "You will  leave this page"; };
+      //   // window.onbeforeunload = function() { return "You will  leave this page"; };
 
-  //   if (window.history && history.pushState) {
-  //     addEventListener('load', function() {
-  //        history.pushState(null, null, null); // creates new history entry with same URL
-  //        addEventListener('popstate', function() {
-  //           var stayOnPage = confirm("Would you like to save this draft?");
-  //           if (!stayOnPage) {
-  //              history.back()
-  //           } else {
-  //              history.pushState(null, null, null);
-  //           }
-  //        });
-  //     });
-  //  }
+      //   if (window.history && history.pushState) {
+      //     addEventListener('load', function() {
+      //        history.pushState(null, null, null); // creates new history entry with same URL
+      //        addEventListener('popstate', function() {
+      //           var stayOnPage = confirm("Would you like to save this draft?");
+      //           if (!stayOnPage) {
+      //              history.back()
+      //           } else {
+      //              history.pushState(null, null, null);
+      //           }
+      //        });
+      //     });
+      //  }
 
-    webix.attachEvent(
-      "onBeforeAjax",
-      function (mode, reqUrl, data, request, headers, files, promise) {
-        promise.then((a) => {
-          const status = a.json().status;
-          const _appUrl = appUrl.url[0].page;
-          if (status === null && _appUrl !== "login") {
+      webix.attachEvent(
+        "onBeforeAjax",
+        function (mode, reqUrl, data, request, headers, files, promise) {
+          promise.then((a) => {
+            const status = a.json().status;
+            const _appUrl = appUrl.url[0].page;
+            if (status === null && _appUrl !== "login") {
+              webix.storage.cookie.remove(COOKIE_NAME);
+              userProfile.token = "";
+              if (!hasReload) {
+                showExpiredMsg(app, true);
+                hasReload = true;
+              }
+            }
+          });
+          promise.catch((b) => {
+            if (
+              b.status == 401 &&
+              !/\/auth\/login.*|\/v1$/.test(request.responseURL)
+            ) {
+              if (!hasReload) {
+                showExpiredMsg(app, true);
+                hasReload = true;
+              }
+            }
+          });
+
+          if (/\/auth\/login.*|\/v1$/.test(request.responseURL)) {
             webix.storage.cookie.remove(COOKIE_NAME);
             userProfile.token = "";
-            if (!hasReload) {
-              showExpiredMsg(app, true);
-              hasReload = true;
-            }
           }
-        });
-        promise.catch((b) => {
-          if (
-            b.status == 401 &&
-            !/\/auth\/login.*|\/v1$/.test(request.responseURL)
-          ) {
-            if (!hasReload) {
-              showExpiredMsg(app, true);
-              hasReload = true;
-            }
+
+          // });
+          if (!webix.storage.cookie.get(COOKIE_NAME)) {
+            // window.location.reload();
+          } else {
+            setAppHeader(headers);
           }
-        });
-
-        if (/\/auth\/login.*|\/v1$/.test(request.responseURL)) {
-          webix.storage.cookie.remove(COOKIE_NAME);
-          userProfile.token = "";
         }
+      );
 
-        // });
-        if (!webix.storage.cookie.get(COOKIE_NAME)) {
-          // window.location.reload();
-        } else {
-          setAppHeader(headers);
-        }
-      }
-    );
+      // webix.ajax(`${API_URL}/app`).then((ra) => {
+      //   const data = ra.json();
+      //   console.log(`profile-ra`, data);
+      //   // state.meta = ra.json();
+      //   state.appProfile = JSON.parse(data.data.find((m)=>m.type==5).content);
+      //   console.log(`d`, state.appProfile);
+      // });
 
-    // webix.ajax(`${API_URL}/app`).then((ra) => {
-    //   const data = ra.json();
-    //   console.log(`profile-ra`, data);
-    //   // state.meta = ra.json();
-    //   state.appProfile = JSON.parse(data.data.find((m)=>m.type==5).content);
-    //   console.log(`d`, state.appProfile);
-    // });
+      initSession();
 
-    initSession();
-    app.render();
+      app.render();
+
+    }, laodingDelay);
   });
 }

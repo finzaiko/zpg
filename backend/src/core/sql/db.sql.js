0b196b98693e4695706ed740d6e961117b495932
Arifin <arifin@citratubindo.com>
2023-03-30 12:53:47 +0700

    upd sample comment func query

--- backend/src/core/sql/db.sql.js
+++ backend/src/core/sql/db.sql.js
@@ -98,54 +98,45 @@ const dbAllViewsBySchema = (schemaOid) => {
 };
 
 const dbFuncContentByOid = (oid) => {
-  let commentDefinition_OLD = `
-      (select CONCAT(
-        '-- FUNCTION: ', isr.specific_schema|| '.' ||  routine_name,'(',string_agg(isp.data_type::text,','),');',
-        e'\n\n',
-        '-- DROP FUNCTION IF EXISTS ',isr.specific_schema|| '.' ||  routine_name,'(',string_agg(isp.data_type::text,','),');',
-        e'\n')
-        as func_def
-        FROM information_schema.routines isr
-        INNER JOIN pg_proc prc ON prc.oid = reverse(split_part(reverse(isr.specific_name), '_', 1))::int
-        INNER JOIN information_schema.parameters isp ON isp.specific_name = isr.specific_name
-        where prc.oid=${oid} AND parameter_mode='IN'
-        group by routine_name, isr.specific_schema)
-    `;
 
-    let commentDefinition = `
-      (WITH a AS (
-        SELECT prc.oid, isp.data_type, isr.specific_schema, isr.routine_name, isp.parameter_mode
+    const commentBase = `
+      WITH a AS (
+        SELECT prc.oid, isp.data_type, isp.parameter_name, isr.specific_schema, isr.routine_name, isp.parameter_mode
         FROM information_schema.routines isr
-        INNER JOIN pg_proc prc ON prc.oid = reverse(split_part(reverse(isr.specific_name), '_', 1))::int
-        INNER JOIN information_schema.parameters isp ON isp.specific_name = isr.specific_name
+        LEFT JOIN pg_proc prc ON prc.oid = reverse(split_part(reverse(isr.specific_name), '_', 1))::int
+        LEFT JOIN information_schema.parameters isp ON isp.specific_name = isr.specific_name
         where prc.oid=${oid}
       ), b AS (
-        SELECT coalesce(string_agg(a.data_type::text,','),'') AS in_params FROM a
+        SELECT
+          coalesce(string_agg(a.data_type::text,','),'') AS in_params_type,
+          coalesce(string_agg(a.parameter_name::text,','),'') AS in_params_name
+        FROM a
         WHERE parameter_mode='IN'
       ), c AS (
         SELECT DISTINCT ON (oid) * FROM a, b
       )
+    `;
+
+    let commentDefinition = `
+      (${commentBase}
       SELECT
       CONCAT(
-        '-- FUNCTION: ', specific_schema|| '.' ||  routine_name,'(',in_params,');',
+        '-- FUNCTION: ', specific_schema|| '.' ||  routine_name,'(',in_params_type,');',
         e'\n\n',
-              '-- DROP FUNCTION IF EXISTS ',specific_schema|| '.' ||  routine_name,'(',in_params,');',
+              '-- DROP FUNCTION IF EXISTS ',specific_schema|| '.' ||  routine_name,'(',in_params_type,');',
         e'\n')
         as func_def
       FROM c)
     `;
 
-  let commentSample = `
-    (SELECT CONCAT(
-        '-- PARAMS NAME:', string_agg(isp.parameter_name::text,',') , e'\n',
-        '-- PARAMS TYPE:', string_agg(isp.data_type::text,',') , e'\n',
-        '-- SELECT * FROM ', isr.specific_schema|| '.' ||  routine_name,'()') as func_def
-    FROM information_schema.routines isr
-    INNER JOIN pg_proc prc ON prc.oid = reverse(split_part(reverse(isr.specific_name), '_', 1))::int
-    INNER JOIN information_schema.parameters isp ON isp.specific_name = isr.specific_name
-    WHERE prc.oid=${oid} AND parameter_mode='IN'
-    GROUP BY routine_name, isr.specific_schema)
-`;
+    let commentSample = `
+      (${commentBase}
+        SELECT CONCAT(
+        '-- PARAMS NAME: ', in_params_type , e'\n',
+        '-- PARAMS TYPE: ', in_params_name , e'\n',
+        '-- SELECT * FROM ', specific_schema|| '.' ||  routine_name,'()') as func_def
+      FROM c)
+  `;
 
   let sql = `
         SELECT CONCAT_WS('',
